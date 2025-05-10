431f9b5db80fecf023bd13735c700b15de696f7c
DS <sda@uniavds.com>
2025-05-10 18:07:48 +0300

    add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    rules: {
+      'no-console': 'off',
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
+      '@stylistic/semi': 'off',
+    },
+  };
+  
