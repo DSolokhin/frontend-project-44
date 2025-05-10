838364c0ff2c361f9d825a952f36260db3dd799f
DS <sda@uniavds.com>
2025-05-10 18:31:07 +0300

    fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,47 @@
+// eslint.config.js
+import pluginJs from '@eslint/js'
+import pluginImport from 'eslint-plugin-import'
+import pluginStylistic from '@stylistic/eslint-plugin'
+
 export default [
-    {
-      files: ['**/*.js'],
-      languageOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-      },
-      env: {
-        node: true,
-        es2021: true,
-      },
-      rules: {
-        'semi': ['error', 'never'],
-        'no-console': 'off',
-        'prefer-const': 'error',
-        'arrow-parens': ['error', 'as-needed'],
-        'import/no-unresolved': 'off',
-        'import/prefer-default-export': 'off',
-      },
-      plugins: {
-        import: require('eslint-plugin-import'),
-      },
-    }
-  ];
+  // Базовые правила от ESLint
+  pluginJs.configs.recommended,
+
+  // Конфигурация импортов
+  {
+    plugins: {
+      import: pluginImport,
+    },
+    rules: {
+      'import/no-unresolved': 'off',
+      'import/prefer-default-export': 'off',
+    },
+  },
+
+  // Стилистические правила
+  {
+    plugins: {
+      '@stylistic': pluginStylistic,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/arrow-parens': ['error', 'as-needed'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/comma-dangle': ['error', 'never'],
+    },
+  },
+
+  // Общие настройки
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    env: {
+      node: true,
+      es2021: true,
+    },
+    files: ['**/*.js'],
+  },
+]
