9862110fb3b4ffb6331843aa56ac8106979b3ba8
DS <sda@uniavds.com>
2025-05-10 17:18:07 +0300

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
-
-// Если используете именованный экспорт (Вариант 1):
-import { greetUser } from '../src/cli.js';
-
-// Если используете default экспорт (Вариант 2):
-// import greetUser from '../src/cli.js';
-
-greetUser();
+import { greetUser } from '../src/cli.js'
+greetUser()
