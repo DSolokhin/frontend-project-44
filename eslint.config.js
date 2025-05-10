cc2692c57d600bb7be56e8f2ab9747bc14775458
DS <sda@uniavds.com>
2025-05-10 18:27:51 +0300

    fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
-export default {
-    rules: {
-      'no-console': 'off',
-      'import/extensions': 'off',
-      'import/prefer-default-export': 'off',
-      '@stylistic/semi': 'off',
-    },
-  };
-  
+export default [
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+      env: {
+        node: true,
+        es2021: true,
+      },
+      rules: {
+        'semi': ['error', 'never'],
+        'no-console': 'off',
+        'prefer-const': 'error',
+        'arrow-parens': ['error', 'as-needed'],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+      },
+      plugins: {
+        import: require('eslint-plugin-import'),
+      },
+    }
+  ];
